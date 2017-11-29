b8299e6fff83140b73eafc1682ae7bee80faa5a1
dominhnhien1997 <nhienminhdo@gmail.com>
2017-11-30 00:18:14 +0700

    Web ban hang cap nhat lan dau

---
+++ WebBanhanh/WebBanhanh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBanhanh.Startup))]
+namespace WebBanhanh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
